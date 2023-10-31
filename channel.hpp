79dca1f9e691febf85d5591a9e14c416b72543b8
Pierre BEHEYT <pierre.beheyt.bba@edhec.com>
2023-10-31 02:08:02 +0100

    create header for channel class

--- channel.hpp
+++ channel.hpp
@@ -1,12 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   channel.hpp                                        :+:      :+:    :+:   */
+/*   channel.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/31 01:09:55 by pbeheyt           #+#    #+#             */
-/*   Updated: 2023/10/31 01:09:56 by pbeheyt          ###   ########.fr       */
+/*   Created: 2023/10/31 01:09:58 by pbeheyt           #+#    #+#             */
+/*   Updated: 2023/10/31 02:04:04 by pbeheyt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+#include <list>
+
+#include "client.hpp"
+
+#ifndef CHANNEL_HPP
+#define CHANNEL_HPP
+
+class Channel {
+	public:
+		Channel(void);
+		Channel(Channel const &rhs);
+		Channel &operator=(Channel const &rhs);
+		~Channel(void);
+		
+		void	addUser(Client const &user);
+		void	delUser(Client const &user);
+	
+		std::string const	&getTopic(void);
+		void				setTopic(std::string const &topicName);
+
+	private:
+		std::string			_topic;
+		std::list<Client *>	_usersList;
+		std::list<Client *>	_operatorsList;
+};
+
+
+#endif
